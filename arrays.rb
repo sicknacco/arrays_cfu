5afaa3331e480c87d8a13eb6ceca7764653c38f8
Nick Sacco <nicksacco10@gmail.com>
2022-12-07 11:40:50 -0800

    Show all strings

---
+++ arrays.rb
@@ -0,0 +1,19 @@
+## Strings
+
+group_members = ["Caroline" , "Kailey" , "Katie" , "Logan"]
+puts group_members[3]
+
+## Integers
+
+age = [21 , 25 , 30 , 32]
+puts age[0]
+
+## Floats
+
+distance = [2.5 , 9.43 , 12.01 , 65.2]
+puts distance[3]
+
+## Booleans
+
+is_this_correct = [ true , false , true , false]
+puts is_this_correct[2]
